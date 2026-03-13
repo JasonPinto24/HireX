77b9e153fa9c986502c336a9f6c08d399e919c63
Carolin Anto <carolinanto906@gmail.com>
2026-03-13 07:29:40 +0530

    added partial skill match

--- src/matching/skill_matcher.py
+++ src/matching/skill_matcher.py
@@ -18,8 +18,11 @@ def embed_skills(skills):
     return model.encode(skills)
 
 
-def match_skills(candidate_skills, job_skills, threshold=0.6):
+def match_skills(candidate_skills, job_skills, min_similarity=0.3):
   
+    #returns partial skill matches based on similarity.
+    #skills with similarity >= min_similarity are considered partially matched.
+    
 
     candidate_embeddings = embed_skills(candidate_skills)
     job_embeddings = embed_skills(job_skills)
@@ -27,35 +30,37 @@ def match_skills(candidate_skills, job_skills, threshold=0.6):
     matched = []
     missing = []
 
+    total_score=0
+
     for i, job_vec in enumerate(job_embeddings):
 
         #enumerate: This allows you to loop through a sequence 
         #and have access to both the index and the element at the same time
-        
+
         similarity_scores = cosine_similarity(
             [job_vec], candidate_embeddings
         )[0]
 
         best_score = max(similarity_scores)
+        total_score+=best_score
+        max_score=len(job_skills)
 
-        if best_score >= threshold:
+        match_percentage = (total_score / max_score) * 100 if max_score > 0 else 0
+
+
+        if best_score >= min_similarity:
             matched.append(job_skills[i])
         else:
             missing.append(job_skills[i])
+
+
 
     return {
         "matched": matched,
         "missing": missing,
         "match_count": len(matched),
-        "total_required": len(job_skills)
+        "total_required": len(job_skills),
+        "match_percentage": round(match_percentage,2)
+        
     }
 
-def calculate_match_percentage(skill_results):
-    
-    total = skill_results["total_required"]
-    matched = skill_results["match_count"]
-
-    if total == 0:
-        return 0
-
-    return (matched / total) * 100
