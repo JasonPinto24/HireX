bad5de672ad5a9715dcabf7738337434c2b78bd7
Vishaal <vishaalkathi@gmail.com>
2026-03-16 23:56:15 +0530

    Modified resume_parser using spacy

--- src/parsing/resume_parser.py
+++ src/parsing/resume_parser.py
@@ -1,5 +1,8 @@
 import fitz
 import re
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 def load_skills(file_path="data/skills_dataset.txt"):
     # load skills from data/skills_dataset
@@ -24,14 +27,24 @@ def extract_text_from_pdf(file_path):
     text = re.sub(r"\s+", " ", text)
     return text.lower()
 
+def extract_candidate_phrases(text):
+    doc = nlp(text)
+    candidates = []
+    for ent in doc.ents:
+        candidates.append(ent.text.lower())
+    
+    for chunk in doc.noun_chunks:
+        candidates.append(chunk.text.lower())
+    
+    return list(set(candidates))
+
 def extract_resume_skills(file_path):
     # create a skills list given in resume
     skills_db = load_skills()
     text = extract_text_from_pdf(file_path)
+    candidates = extract_candidate_phrases(text)
     found_skills = []
-    for skill in skills_db:
-        # create regex pattern of all skills to search in resume text
-        pattern = r"\b(" + "|".join(map(re.escape, skills_db)) + r")\b"
-        matches = re.findall(pattern, text)
-        found_skills = list(set(matches))
+    for candidate in candidates:
+        if candidate in skills_db:
+            found_skills.append(candidate)
     return found_skills
